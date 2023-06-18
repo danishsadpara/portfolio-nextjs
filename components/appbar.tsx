3fdc95c96fad5200e50b4b44238b13e97d26e5a8
danishsadpara <danishsadpara@gmail.com>
2023-06-18 12:55:35 +0500

    added navbar and make it responsive

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -3,126 +3,101 @@ import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import Button from '@mui/material/Button';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from '@mui/material';
 
 const GlassmorphicAppBar = styled(AppBar)`
   background: rgba(255, 255, 255, 0.15);
   backdrop-filter: blur(10px);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  box-shadow: 0 4px 12px 0 rgba(31, 38, 135, 0.05);
+`;
+
+const GlassyButton = styled(Button)`
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(10px);
+  box-shadow: 0 8px 12px 0 rgba(31, 38, 135, 0.04);
+  color: black;
+  margin-right: 10px;
+  &:hover {
+    background: rgba(255, 225, 255, 0.3);
+  }
 `;
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [value, setValue] = React.useState(0);
+  const [menuOpen, setMenuOpen] = React.useState(false);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
+  const handleChange = (event:any, newValue:any) => {
+    setValue(newValue);
   };
 
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
+  const handleContactUs = () => {
+    console.log('Contact Us clicked');
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <GlassmorphicAppBar position="static">
+    <GlassmorphicAppBar>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 0 }}>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                fontFamily: 'Proxima Nova',
-                fontWeight: 700,
-                color: 'black',
-                textDecoration: 'none',
-              }}
-            >
-              @danishsadpara
-            </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              flexGrow: 1,
+              mr: 2,
+              fontFamily: 'Roboto',
+              fontWeight: 700,
+              color: 'black',
+              textDecoration: 'none',
+            }}
+          >
+            danishsadpara
+          </Typography>
+          <Box sx={{ flexGrow: 0, marginLeft: 'auto' }}>
+            <GlassyButton onClick={handleContactUs}>Contact Us</GlassyButton>
           </Box>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
+          <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+            <IconButton onClick={handleMenuToggle} color="error">
               <MenuIcon />
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {/* Add your menu items here */}
-            </Menu>
+            {menuOpen && (
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="Navigation tabs"
+                orientation="vertical"
+                sx={{
+                  position: 'absolute',
+                  top: '100%',
+                  right: 0,
+                  left: 0,
+                  backgroundColor: 'white',
+                  zIndex: 1000,
+                }}
+              >
+                <Tab label="Home" />
+                <Tab label="Projects" />
+                <Tab label="Skills" />
+              </Tabs>
+            )}
           </Box>
-
-          <Box sx={{ flexGrow: 1 }} />
-
-          <Box sx={{ flexGrow: 0 }}>
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-            </IconButton>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Tabs value={value} onChange={handleChange} aria-label="Navigation tabs">
+              <Tab label="Home" />
+              <Tab label="Projects" />
+              <Tab label="Skills" />
+            </Tabs>
           </Box>
         </Toolbar>
       </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 const Index = () => {
   return (
-    <div className="glassmorphic-container">
-    <h1 className="blinking-text">Danish Sadpara</h1>
-  </div>
+    <>
+    </>
   );
 };
 
